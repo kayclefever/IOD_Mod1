81f7c8a313a69234f4b97a84a3db86c69efda461
kayclefever <kayclefever3342@gmail.com>
2024-09-26 20:00:35 -0700

    practice

--- backend/helloworld.js
+++ backend/helloworld.js
@@ -1,3 +1,8 @@
+const Sentiment = require('sentiment');
+const sentiment = new Sentiment();
+const result = sentiment.analyze('Cats are stupid.');
+console.log(result);    // Score: -2, Comparative: -0.666
+
 console.log('Hello World!');
 function helloworld(name){
     return `Hello ${name}`;
