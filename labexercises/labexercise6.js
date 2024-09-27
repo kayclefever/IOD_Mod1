e48eb754630124b785d0ddae92871f43bcf766af
kayclefever <kayclefever3342@gmail.com>
2024-09-26 20:35:51 -0700

    json book

---
+++ labexercises/labexercise6.js
@@ -0,0 +1,14 @@
+const book= {
+    "title" : "Can't Hurt Me",
+    "description" : "Master your mind and defy the odds",
+    "author" : "David Goggins",
+    "number_of_pages" : 363
+};
+
+console.log(book.title);
+console.log(book.description);
+console.log(book.author);
+console.log(book.pages);
+console.log(book);
+book.description = "discipline your life";
+console.log(book);
