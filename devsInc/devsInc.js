189eeabd865e0b0de62b395c9e3c1498946a177a
kayclefever <kayclefever3342@gmail.com>
2024-09-28 15:38:11 -0700

    exercise 7

---
+++ devsInc/devsInc.js
@@ -0,0 +1,12 @@
+function rollDice(){
+    const numOfDice = document.getElementById("numOfDice").value;
+    const diceResult = document.getElementById("diceResult");
+    const diceImages = document.getElementById("diceImages");
+    const values =[];
+    const images =[];
+
+    for(let i = 0; i < numofDice; i++){
+        const value = Math.floor() * 6) + 1;
+        console.log(value);
+    }
+}
