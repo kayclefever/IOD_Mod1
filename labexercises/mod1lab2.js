22f263202141afaf982ada62e09a507511bb9a49
kayclefever <kayclefever3342@gmail.com>
2024-10-12 13:37:26 -0700

    lab 2

---
+++ labexercises/mod1lab2.js
@@ -0,0 +1,25 @@
+function sum(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+console.log(sum(10, 5));
+console.log(subtract(10, 5));
+console.log(multiply(10, 5));
+console.log(divide(10, 5));
+
+function hello(name) {
+  return `Hello ${name}`;
+}
+console.log(hello("KayC"));
