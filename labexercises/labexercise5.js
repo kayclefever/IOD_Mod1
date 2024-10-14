9e7e85135c68974bbd485e1d103f2dd4272706d9
kayclefever <kayclefever3342@gmail.com>
2024-10-14 11:47:40 -0700

    lab 5

--- labexercises/labexercise5.js
+++ labexercises/labexercise5.js
@@ -1,14 +1,79 @@
-let basketballplayers = ['Jimmy Butler', 'Kobe Bryant', 'Dwyane Wade', 'LeBron James', 'Steph Curry'];
+//Adds two numbers together
+function sum(a, b) {}
+console.log(sum(10, 5));
 
-basketballplayers[1] = 'Michael Jordan';
-console.log(basketballplayers);
+if (sum(10, 5) != 15) {
+  throw new Error("false");
+}
 
-basketballplayers[4] = 'Klay Thompson';
-console.log(basketballplayers);
+if (sum(10, 5) == 0) {
+  throw new Error("false");
+}
 
-basketballplayers.unshift('Kyrie Irving');
-console.log(basketballplayers);
+if (sum(10, 5) == -4) {
+  throw new Error("false");
+}
 
-let lastBasketballplayer = basketballplayers.pop();
-console.log(lastBasketballplayer);
-console.log(basketballplayers);
+function sum(a, b) {
+  return a + b;
+}
+
+//Subtracts two numbers
+function subtract(a, b) {}
+console.log(subtract(10, 5));
+
+if (subtract(10, 5) != 5) {
+  throw new Error("false");
+}
+
+if (subtract(10, 5) == 0) {
+  throw new Error("false");
+}
+
+if (subtract(10, 5) == -4) {
+  throw new Error("false");
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+//Multiplies two numbers
+function multiply(a, b) {}
+console.log(multiply(10, 5));
+
+if (multiply(10, 5) != 50) {
+  throw new Error("false");
+}
+
+if (multiply(10, 5) == 0) {
+  throw new Error("false");
+}
+
+if (multiply(10, 5) == -4) {
+  throw new Error("false");
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+//Divides two numbers
+function divide(a, b) {}
+console.log(divide(10, 5));
+
+if (sum(10, 5) != 2) {
+  throw new Error("false");
+}
+
+if (sum(10, 5) == 0) {
+  throw new Error("false");
+}
+
+if (sum(10, 5) == -4) {
+  throw new Error("false");
+}
+
+function divide(a, b) {
+  return a / b;
+}
